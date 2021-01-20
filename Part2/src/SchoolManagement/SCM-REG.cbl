0836a504a7a74b8e8d30625831f5e3ba88914c08
cscosta95 <75365445+cscosta95@users.noreply.github.com>
2021-01-20 17:32:17 +0000

    Update
    Progress at the Registration Module

--- Part2/src/SchoolManagement/SCM-REG.cbl
+++ Part2/src/SchoolManagement/SCM-REG.cbl
@@ -78,52 +78,125 @@
 
        SCREEN SECTION.
        01  CLEAR-SCREEN BACKGROUND-COLOR 0.
-           03 VALUE " " BLANK SCREEN LINE 01 COL 01.
+           05 VALUE " " BLANK SCREEN LINE 01 COL 01.
 
        01  MAIN-SCREEN
            BACKGROUND-COLOR 7, FOREGROUND-COLOR 0.
-           03 VALUE ALL " " PIC X(120) LINE 02 COL 01.
-           03 VALUE ALL " " PIC X(120) LINE 03 COL 01.
-           03 VALUE ALL " " PIC X(120) LINE 04 COL 01.
-           03 VALUE "SCHOOL MANAGEMENT" LINE 03 COL 50.
-           03 VALUE ALL " " PIC X(120) LINE 24 COL 01.
-           03 VALUE ALL " " PIC X(120) LINE 25 COL 01.
-           03 VALUE ALL " " PIC X(120) LINE 26 COL 01.
-           03 VALUE "F3 - BACK | F4 - EXIT"
-           LINE 25 COL 99 FOREGROUND-COLOR 5.
-           03 VALUE "  " LINE 24 COL 96 BACKGROUND-COLOR 0.
-           03 VALUE "  " LINE 25 COL 96 BACKGROUND-COLOR 0.
-           03 VALUE "  " LINE 26 COL 96 BACKGROUND-COLOR 0.
+           05 VALUE ALL " " PIC X(120) LINE 02 COL 01.
+           05 VALUE ALL " " PIC X(120) LINE 03 COL 01.
+           05 VALUE ALL " " PIC X(120) LINE 04 COL 01.
+           05 VALUE "SCHOOL MANAGEMENT" LINE 03 COL 50.
+           05 VALUE ALL " " PIC X(120) LINE 24 COL 01.
+           05 VALUE ALL " " PIC X(120) LINE 25 COL 01.
+           05 VALUE ALL " " PIC X(120) LINE 26 COL 01.
+           05 VALUE "F3 - BACK | F4 - EXIT"
+               LINE 25 COL 99 FOREGROUND-COLOR 5.
+           05 VALUE "  " LINE 24 COL 96 BACKGROUND-COLOR 0.
+           05 VALUE "  " LINE 25 COL 96 BACKGROUND-COLOR 0.
+           05 VALUE "  " LINE 26 COL 96 BACKGROUND-COLOR 0.
 
-       01  REGISTER-MENU
+       01  MAIN-REGISTER-SCREEN
            BACKGROUND-COLOR 7, FOREGROUND-COLOR 0, AUTO, REQUIRED.
-           03 VALUE ALL " " PIC X(50) LINE 09 COL 35.
-           03 VALUE ALL " " PIC X(50) LINE 10 COL 35.
-           03 VALUE ALL " " PIC X(50) LINE 11 COL 35.
-           03 VALUE ALL " " PIC X(50) LINE 12 COL 35.
-           03 VALUE ALL " " PIC X(50) LINE 13 COL 35.
-           03 VALUE ALL " " PIC X(50) LINE 14 COL 35.
-           03 VALUE ALL " " PIC X(50) LINE 15 COL 35.
-           03 VALUE ALL " " PIC X(50) LINE 16 COL 35.
-           03 VALUE "1 - Register School Manually" LINE 11 COL 42.
-           03 VALUE "2 - Register School Through CSV File"
+           05 VALUE ALL " " PIC X(50) LINE 09 COL 35.
+           05 VALUE ALL " " PIC X(50) LINE 10 COL 35.
+           05 VALUE ALL " " PIC X(50) LINE 11 COL 35.
+           05 VALUE ALL " " PIC X(50) LINE 12 COL 35.
+           05 VALUE ALL " " PIC X(50) LINE 13 COL 35.
+           05 VALUE ALL " " PIC X(50) LINE 14 COL 35.
+           05 VALUE ALL " " PIC X(50) LINE 15 COL 35.
+           05 VALUE ALL " " PIC X(50) LINE 16 COL 35.
+           05 VALUE "1 - Register School Manually" LINE 11 COL 42.
+           05 VALUE "2 - Register School Through CSV File"
                LINE 12 COL 42.
-           03 VALUE "3 - Back to Main Menu" LINE 13 COL 42.
-           03 VALUE "What do you wish to do:" LINE 20 COL 45
+           05 VALUE "3 - Back to Main Menu" LINE 13 COL 42.
+           05 VALUE "What do you wish to do:" LINE 20 COL 45
            REVERSE-VIDEO.
-           03 MP-OPTION PIC 9(02) LINE 20 COL 68 TO WS-OPTION
+           05 MP-OPTION PIC 9(02) LINE 20 COL 68 TO WS-OPTION
                BLANK WHEN ZERO REVERSE-VIDEO.
+
+       01  REGISTER-SCREEN
+           BACKGROUND-COLOR 0, FOREGROUND-COLOR 7.
+           05 VALUE "Register New School" LINE 9 COL 25.
+           05 VALUE "Internal ID:" LINE 11 COL 26.
+           05 VALUE "External ID:" LINE 12 COL 26.
+           05 VALUE "Designation1:" LINE 13 COL 25.
+           05 VALUE "Designation2:" LINE 14 COL 25.
+           05 VALUE "Designation3:" LINE 15 COL 25.
+           05 VALUE "Address1:" LINE 16 COL 29.
+           05 VALUE "Address2:" LINE 17 COL 29.
+           05 VALUE "Postal Code:" LINE 18 COL 25.
+           05 VALUE "-" LINE 18 COL 44.
+           05 VALUE "Town:" LINE 19 COL 25.
+           05 REG-REC.
+               10 REG-IID PIC 9(003) LINE 11 COL 39
+                   TO WS-SCHOOL-INTERNAL-ID
+                   BLANK WHEN ZERO.
+               10 REG-EED PIC X(008) LINE 12 COL 39
+                   TO WS-SCHOOL-EXTERNAL-ID.
+               10 REG-DESIGNATION.
+                   15 REG-DESIGNATION1 PIC X(050) LINE 13 COL 39
+                       TO WS-SCHOOL-DESIGNATION1.
+                   15 REG-DESIGNATION2 PIC X(050) LINE 14 COL 39
+                       TO WS-SCHOOL-DESIGNATION2.
+                   15 REG-DESIGNATION3 PIC X(050) LINE 15 COL 39
+                       TO WS-SCHOOL-DESIGNATION3.
+               10 REG-ADDRESS.
+                   15 REG-ADDRESS1 PIC X(050) LINE 16 COL 39
+                       TO WS-SCHL-ADR-MAIN1.
+                   15 REG-ADDRESS2 PIC X(050) LINE 17 COL 39
+                       TO WS-SCHL-ADR-MAIN2.
+               10 REG-POSTAL-CODE.
+                   15 REG-PC1 PIC 9(004) LINE 18 COL 39
+                       TO WS-SCHL-POSTAL-CODE1
+                       BLANK WHEN ZERO.
+                   15 REG-PC2 PIC 9(003) LINE 18 COL 46
+                       TO WS-SCHL-POSTAL-CODE2
+                       BLANK WHEN ZERO.
+               10 REG-TOWN PIC X(030) LINE 19 COL 39
+                   TO WS-SCHOOL-TOWN.
+           05 VALUE ALL " " PIC X(80) LINE 7 COL 18
+               BACKGROUND-COLOR 7.
+           05 VALUE ALL " " PIC X(80) LINE 21 COL 18
+               BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 20 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 8 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 9 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 10 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 11 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 12 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 13 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 14 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 15 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 16 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 17 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 18 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 19 COL 18 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 20 COL 96 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 8 COL 96 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 9 COL 96 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 10 COL 96 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 11 COL 96 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 12 COL 96 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 13 COL 96 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 14 COL 96 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 15 COL 96 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 16 COL 96 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 17 COL 96 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 18 COL 96 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 19 COL 96 BACKGROUND-COLOR 7.
+
+
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
 
            PERFORM CHECK-FILE
 
-           PERFORM WITH TEST AFTER UNTIL OPTION-VLD
+           PERFORM WITH TEST AFTER UNTIL WS-OPTION = 3
                MOVE ZERO TO MP-OPTION
                DISPLAY CLEAR-SCREEN
                DISPLAY MAIN-SCREEN
-               DISPLAY REGISTER-MENU
+               DISPLAY MAIN-REGISTER-SCREEN
                ACCEPT MP-OPTION
 
                EVALUATE WS-OPTION
@@ -133,12 +206,17 @@
                    WHEN 2
                        PERFORM REGISTER-CSV
 
-                   WHEN 3
-                       EXIT PROGRAM
+           END-EVALUATE
 
            END-PERFORM.
 
+           EXIT PROGRAM.
+
        REGISTER-MANUAL SECTION.
+
+           DISPLAY CLEAR-SCREEN
+           DISPLAY MAIN-SCREEN
+           ACCEPT REGISTER-SCREEN.
 
        REGISTER-INTERNAL-ID.
            OPEN INPUT KEYS
