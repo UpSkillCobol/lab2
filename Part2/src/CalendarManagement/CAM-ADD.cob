e780710367c4810b4b63b1cd030699b4e5af84f8
Diogo Lima <lsodiogo@upskill.pt>
2021-01-27 21:56:37 +0000

    UPDATE
    UPDATE IN SOURCE CODES.
    ALL DATE VERIFICATIONS ARE DONE.
    ANOTHER COPYBOOK CREATED.
    #20 27.01.2020(3)

--- Part2/src/CalendarManagement/CAM-ADD.cob
+++ Part2/src/CalendarManagement/CAM-ADD.cob
@@ -3,7 +3,7 @@
       ******************************************************************
       *    BREADWICH | CALENDAR MANAGEMENT
       ******************************************************************
-      *    ADD MODULE | V0.5 | IN UPDATE | 27.01.2020
+      *    ADD MODULE | V0.6 | IN UPDATE | 27.01.2020
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. CAM-ADD.
@@ -36,14 +36,7 @@
        WORKING-STORAGE SECTION.
        COPY LANGUAGE.
        COPY WSCALENDAR.
-
-       01  SAVE                                 PIC X(002).
-           88 SAVE-YES                          VALUE "Y" "y".
-           88 SAVE-NO                           VALUE "N" "n".
-           88 SAVE-VALID                        VALUE "Y" "y" "N" "n".
-       77  CALENDAR-TEST                        PIC 9(002).
-       77  KEYS-TEST                            PIC 9(002).
-       77  PRESS-KEY                            PIC X.
+       COPY WSVAR.
 
        SCREEN SECTION.
        01  CLEAR-SCREEN.
@@ -95,34 +88,34 @@
            05 VALUE "  " LINE 18 COL 96 BACKGROUND-COLOR 7.
            05 VALUE "  " LINE 19 COL 96 BACKGROUND-COLOR 7.
            05 VALUE REGISTER-TEXT             LINE 09 COL 48.
-           05 VALUE REGISTER-TEXT-ID          LINE 13 COL 22.
-           05 VALUE REGISTER-TEXT-DATE        LINE 15 COL 22.
-           05 VALUE REGISTER-TEXT-DATE1       LINE 16 COL 22.
-           05 VALUE REGISTER-TEXT-DESCRIPTION LINE 18 COL 22.
-           05 REG-ID PIC 9(003) LINE 13 COL 44 USING WS-DOWNTIME-ID.
+           05 VALUE REGISTER-TEXT-ID          LINE 13 COL 21.
+           05 VALUE REGISTER-TEXT-DATE        LINE 15 COL 21.
+           05 VALUE REGISTER-TEXT-DATE1       LINE 16 COL 21.
+           05 VALUE REGISTER-TEXT-DESCRIPTION LINE 18 COL 21.
+           05 REG-ID PIC 9(003) LINE 13 COL 45 USING WS-DOWNTIME-ID.
            05 REG-REC.
               10 REG-START-DATE.
-                 15 REG-START-DAY PIC 9(002) LINE 15 COL 44 TO
+                 15 REG-START-DAY PIC 9(002) LINE 15 COL 45 TO
                     WS-START-DT-DAY BLANK WHEN ZERO AUTO REQUIRED.
-                 15 LINE 15 COL 46 VALUE "/".
-                 15 REG-START-MONTH PIC 9(002) LINE 15 COL 47 TO
+                 15 LINE 15 COL 47 VALUE "/".
+                 15 REG-START-MONTH PIC 9(002) LINE 15 COL 48 TO
                     WS-START-DT-MONTH BLANK WHEN ZERO AUTO REQUIRED.
-                 15 LINE 15 COL 49 VALUE "/".
-                 15 REG-START-YEAR PIC 9(004) LINE 15 COL 50 TO
+                 15 LINE 15 COL 50 VALUE "/".
+                 15 REG-START-YEAR PIC 9(004) LINE 15 COL 51 TO
                     WS-START-DT-YEAR BLANK WHEN ZERO AUTO REQUIRED.
               10 REG-END-DATE.
-                 15 REG-END-DAY PIC 9(002) LINE 16 COL 44 TO
+                 15 REG-END-DAY PIC 9(002) LINE 16 COL 45 TO
                     WS-END-DT-DAY BLANK WHEN ZERO AUTO.
-                 15 LINE 16 COL 46 VALUE "/".
-                 15 REG-END-MONTH PIC 9(002) LINE 16 COL 47 TO
+                 15 LINE 16 COL 47 VALUE "/".
+                 15 REG-END-MONTH PIC 9(002) LINE 16 COL 48 TO
                     WS-END-DT-MONTH BLANK WHEN ZERO AUTO.
-                 15 LINE 16 COL 49 VALUE "/".
-                 15 REG-END-YEAR PIC 9(004) LINE 16 COL 50 TO
+                 15 LINE 16 COL 50 VALUE "/".
+                 15 REG-END-YEAR PIC 9(004) LINE 16 COL 51 TO
                     WS-END-DT-YEAR BLANK WHEN ZERO AUTO.
               10 REG-DESCRIPTION.
-                 15 REG-DESCRIPTION1 PIC X(050) LINE 18 COL 44
+                 15 REG-DESCRIPTION1 PIC X(050) LINE 18 COL 45
                     TO WS-DOWNTIME-DESCRIPTION1 AUTO.
-                 15 REG-DESCRIPTION2 PIC X(050) LINE 19 COL 44
+                 15 REG-DESCRIPTION2 PIC X(050) LINE 19 COL 45
                     TO WS-DOWNTIME-DESCRIPTION2 AUTO.
 
        01  INVALID-SCREEN BACKGROUND-COLOR 7 FOREGROUND-COLOR 0.
@@ -137,7 +130,7 @@
            05 VALUE ALL " " PIC X(095) LINE 24 COL 01.
            05 VALUE ALL " " PIC X(095) LINE 25 COL 01.
            05 VALUE ALL " " PIC X(095) LINE 26 COL 01.
-           05 INSTRUCTIONS-TEXT LINE 25 COL 03 PIC X(085)
+           05 INSTRUCTIONS-TEXT LINE 25 COL 03 PIC X(092)
               FOREGROUND-COLOR 4 BACKGROUND-COLOR 7.
 
        01  SAVE-SCREEN
@@ -179,66 +172,72 @@
                  ACCEPT INVALID-SCREEN
               END-IF
            END-PERFORM
-              IF SAVE = "Y" OR "y"
-                 REWRITE FDKEYS
-                 END-REWRITE
-                 WRITE FD-CALENDAR FROM WS-CALENDAR
-                 END-WRITE
-                 CLOSE CALENDAR
+           IF SAVE = "Y" OR "y"
+              REWRITE FDKEYS
+              END-REWRITE
+              WRITE FD-CALENDAR FROM WS-CALENDAR
+              END-WRITE
+              CLOSE KEYS
+              CLOSE CALENDAR
+              MOVE MESSAGE-WRITE-YES TO INVALID-TEXT
+              ACCEPT INVALID-SCREEN
+           ELSE
+              IF SAVE = "N" OR "n"
                  CLOSE KEYS
-                 MOVE MESSAGE-WRITE-YES TO INVALID-TEXT
+                 CLOSE CALENDAR
+                 MOVE MESSAGE-WRITE-NO TO INVALID-TEXT
                  ACCEPT INVALID-SCREEN
-              ELSE
-                 IF SAVE = "N" OR "n"
-                    CLOSE KEYS CALENDAR
-                    MOVE MESSAGE-WRITE-NO TO INVALID-TEXT
-                    ACCEPT INVALID-SCREEN
-                 END-IF
               END-IF
-
+           END-IF
            EXIT PROGRAM.
 
        DOWNTIME-ID SECTION.
            OPEN I-O KEYS
               READ KEYS
                  ADD 1 TO FDKEYS
-
            EXIT SECTION.
 
        DOWNTIME-START-DATE SECTION.
-           PERFORM WITH TEST AFTER UNTIL VALID-DAY AND VALID-MONTH
-           AND VALID-YEAR
+           MOVE SPACE TO DATE-VALID
+           PERFORM WITH TEST AFTER UNTIL DATE-VALID = "Y"
               MOVE ZEROS TO REG-START-DAY, REG-START-MONTH,
               REG-START-YEAR
+              DISPLAY REG-START-DATE
               MOVE INSTRUCTIONS-DATE TO INSTRUCTIONS-TEXT
               DISPLAY INSTRUCTIONS-SCREEN
               ACCEPT REG-START-DAY
+              MOVE   REG-START-DAY   TO WS-DAY
               ACCEPT REG-START-MONTH
+              MOVE   REG-START-MONTH TO WS-MONTH
               ACCEPT REG-START-YEAR
-              IF NOT VALID-DAY OR NOT VALID-MONTH OR NOT VALID-YEAR THEN
-                 MOVE INVALID-DATE TO INVALID-TEXT
-                 ACCEPT INVALID-SCREEN
-              END-IF
+              MOVE   REG-START-YEAR  TO WS-YEAR
+              ACCEPT WS-CURRENT-DATE FROM DATE YYYYMMDD
+              PERFORM CHECK-DATE
            END-PERFORM
-
+           MOVE WS-DAY TO WS-START-DT-DAY
+           MOVE WS-MONTH TO WS-START-DT-MONTH
+           MOVE WS-YEAR TO WS-START-DT-YEAR
            EXIT SECTION.
 
        DOWNTIME-END-DATE SECTION.
-           PERFORM WITH TEST AFTER UNTIL VALID-DAY1 AND VALID-MONTH1
-           AND VALID-YEAR1
+           MOVE SPACE TO DATE-VALID
+           PERFORM WITH TEST AFTER UNTIL DATE-VALID = "Y"
               MOVE ZEROS TO REG-END-DAY, REG-END-MONTH, REG-END-YEAR
+              DISPLAY REG-END-DATE
               MOVE INSTRUCTIONS-DATE TO INSTRUCTIONS-TEXT
               DISPLAY INSTRUCTIONS-SCREEN
               ACCEPT REG-END-DAY
+              MOVE   REG-END-DAY   TO WS-DAY
               ACCEPT REG-END-MONTH
+              MOVE   REG-END-MONTH TO WS-MONTH
               ACCEPT REG-END-YEAR
-              IF NOT VALID-DAY1 OR NOT VALID-MONTH1 OR NOT VALID-YEAR1
-              THEN
-                 MOVE INVALID-DATE TO INVALID-TEXT
-                 ACCEPT INVALID-SCREEN
-              END-IF
+              MOVE   REG-END-YEAR  TO WS-YEAR
+              ACCEPT WS-CURRENT-DATE FROM DATE YYYYMMDD
+              PERFORM CHECK-DATE
            END-PERFORM
-
+           MOVE WS-DAY TO WS-END-DT-DAY
+           MOVE WS-MONTH TO WS-END-DT-MONTH
+           MOVE WS-YEAR TO WS-END-DT-YEAR
            EXIT SECTION.
 
        DOWNTIME-DESCRIPTION SECTION.
@@ -246,7 +245,6 @@
            MOVE INSTRUCTIONS-DESCRIPTION TO INSTRUCTIONS-TEXT
            DISPLAY INSTRUCTIONS-SCREEN
            ACCEPT REG-DESCRIPTION
-
            EXIT SECTION.
 
        CREATE-FILE SECTION.
@@ -268,7 +266,50 @@
            ELSE
               CLOSE KEYS
            END-IF
+           EXIT SECTION.
 
+       CHECK-DATE SECTION.
+           IF VALID-YEAR AND VALID-MONTH AND VALID-DAY THEN
+              IF WS-DAY >= WS-CURRENT-DAY AND WS-MONTH >=
+              WS-CURRENT-MONTH AND WS-YEAR >= WS-CURRENT-YEAR THEN
+                 MOVE "Y" TO DATE-VALID
+                 IF NOT MONTH-FEB AND NOT MONTH-30 THEN
+                    MOVE "Y" TO DATE-VALID
+                 ELSE
+                    IF MONTH-30 AND DAY-30 THEN
+                       MOVE "Y" TO DATE-VALID
+                    END-IF
+                    IF MONTH-FEB THEN
+                       PERFORM LEAP-YEAR-CHECK
+                       IF LEAP-YEAR-YES AND FEB-LEAP-YEAR THEN
+                          MOVE "Y" TO DATE-VALID
+                       ELSE
+                          IF NOT LEAP-YEAR-YES AND DAY-FEBRUARY THEN
+                             MOVE "Y" TO DATE-VALID
+                          END-IF
+                       END-IF
+                    END-IF
+                 END-IF
+              END-IF
+           END-IF
+
+           IF DATE-VALID NOT = "Y" THEN
+              MOVE INVALID-DATE TO INVALID-TEXT
+              ACCEPT INVALID-SCREEN
+           END-IF
+           EXIT SECTION.
+
+       LEAP-YEAR-CHECK SECTION.
+           MOVE SPACE TO LEAP-YEAR
+           IF FUNCTION MOD (WS-YEAR,4) = 0 THEN
+              IF FUNCTION MOD (WS-YEAR,100) <> 0 THEN
+                 MOVE "Y" TO LEAP-YEAR
+              ELSE
+                 IF FUNCTION MOD (WS-YEAR,400) = 0 THEN
+                    MOVE "Y" TO LEAP-YEAR
+                 END-IF
+               END-IF
+           END-IF
            EXIT SECTION.
 
        END PROGRAM CAM-ADD.
--- Part2/src/CalendarManagement/WSCALENDAR.cpy
+++ Part2/src/CalendarManagement/WSCALENDAR.cpy
@@ -2,18 +2,12 @@
            05  WS-DOWNTIME-ID                      PIC 9(003).
            05  WS-START-DOWNTIME.
                10  WS-START-DT-DAY                 PIC 9(002).
-                   88  VALID-DAY                   VALUE 1 THRU 31.
                10  WS-START-DT-MONTH               PIC 9(002).
-                   88  VALID-MONTH                 VALUE 1 THRU 12.
                10  WS-START-DT-YEAR                PIC 9(004).
-                   88 VALID-YEAR                   VALUE 2021 THRU 2022.
            05  WS-END-DOWNTIME.
                10  WS-END-DT-DAY                   PIC 9(002).
-                   88  VALID-DAY1                  VALUE 1 THRU 31.
                10  WS-END-DT-MONTH                 PIC 9(002).
-                   88  VALID-MONTH1                VALUE 1 THRU 12.
                10  WS-END-DT-YEAR                  PIC 9(004).
-                   88 VALID-YEAR1                  VALUE 2021 THRU 2022.
            05  WS-DOWNTIME-DESCRIPTION.
                10  WS-DOWNTIME-DESCRIPTION1        PIC X(050).
                10  WS-DOWNTIME-DESCRIPTION2        PIC X(050).
