73bf513136f35af372a8ab8f17f2647cd261a5ca
Bruno Lopes <75369837+lopesnext3@users.noreply.github.com>
2021-02-04 00:12:47 +0000

    update modify
    #23

--- Part2/src/IngredientsManagement/MODIFY-INGREDS.cbl
+++ Part2/src/IngredientsManagement/MODIFY-INGREDS.cbl
@@ -1,17 +1,413 @@
       ******************************************************************
-      * Author:
-      * Date:
-      * Purpose:
-      * Tectonics: cobc
+      *    LAB | SECOND PART | DELICIOUSSANDWICH
       ******************************************************************
+      *    BREADWICH | INGREDIENTS MANAGEMENT
+      ******************************************************************
+      *    INGREDIENTS MODULE - MODIFY INGREDIENTS
+      ******************************************************************
+      *     V1 | EM ATUALIZAÇÃO | 03.01.2020
+      ******************************************************************
+
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       PROGRAM-ID. MODIFY-INGREDS.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+       CRT STATUS IS KEYSTATUS.
+       REPOSITORY.
+       FUNCTION ALL INTRINSIC.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+               SELECT FXINGRED ASSIGN TO "FXINGREDS"
+                   ORGANIZATION IS INDEXED
+                   ACCESS MODE IS DYNAMIC
+                   RECORD KEY IS INGREDS-ID
+                   ALTERNATE KEY IS INGREDS-NAME
+                   FILE STATUS INGRED-STATUS.
        DATA DIVISION.
        FILE SECTION.
+       FD FXINGRED.
+
+       COPY FD-INGREDSFX.
+
        WORKING-STORAGE SECTION.
+
+       COPY CONSTANTS-INGREDS.
+
+       COPY WS-INGREDSFX.
+
+       01  EDIT-OPTION                         PIC X(002).
+           88 EDIT-VALID-OPTION                VALUE "Y" "y" "N" "n".
+           88 EDIT-OPTION-NO                   VALUE "N" "n".
+       77  DUMMY                               PIC X(001).
+       77  INGRED-STATUS                       PIC 9(002).
+       77  KEYSTATUS                           PIC 9(004).
+       77  FXKEY-STATUS                        PIC 9(002).
+       01  SAVE-IT1                            PIC X(002).
+           88 SAVE-IT1-YES                     VALUE "Y" "y".
+           88 SAVE-IT1-VALID                   VALUE "Y" "y" "N" "n".
+       01  GET-VALID-ID                        PIC 9(003).
+           88 VALID-ID                         VALUE 1 THRU 999.
+       01  INGREDEXIST                           PIC X(002).
+           88 INGREDEXIST-YES                    VALUE "Y".
+       01  EDIT-WHAT                           PIC 9(001).
+           88 EDIT-WHAT-EXIT                   VALUE 8.
+       77 UNSTR                        PIC X(150).
+       77 UNSTR1                       PIC X(050).
+       77 UNSTR2                       PIC X(050).
+       77 UNSTR3                       PIC X(050).
+       77 UNSTR4                       PIC X(050).
+       77 UNSTR5                       PIC X(050).
+       77 UNSTR6                       PIC X(050).
+       77 UNSTR7                       PIC X(050).
+       77 UNSTR8                       PIC X(050).
+       77 UNSTR9                       PIC X(050).
+       77 UNSTR10                      PIC X(050).
+       77 ILIN                         PIC 9(002).
+       77 ICOL                         PIC 9(002).
+       77 EOF                          PIC X(001).
+       77 TRUE-YES                     PIC X(001).
+
+
+       SCREEN SECTION.
+      ******************************************************************
+       01  CLEAR-SCREEN.
+           03 BLANK SCREEN.
+      ******************************************************************
+       01  MAIN-SCREEN
+           BACKGROUND-COLOR 7 FOREGROUND-COLOR 0.
+           05 VALUE ALL " " PIC X(120) LINE 02 COL 01.
+           05 VALUE ALL " " PIC X(120) LINE 03 COL 01.
+           05 VALUE ALL " " PIC X(120) LINE 04 COL 01.
+           05 VALUE MODULE-NAME-MODIFY LINE 03 COL 50.
+           05 VALUE ALL " " PIC X(95) LINE 24 COL 01.
+           05 VALUE ALL " " PIC X(95) LINE 25 COL 01.
+           05 VALUE ALL " " PIC X(95) LINE 26 COL 01.
+           05 VALUE ALL " " PIC X(22) LINE 24 COL 98.
+           05 VALUE ALL " " PIC X(22) LINE 25 COL 98.
+           05 VALUE ALL " " PIC X(22) LINE 26 COL 98.
+           05 VALUE BACK-EXIT LINE 25 COL 99 FOREGROUND-COLOR 5.
+      ******************************************************************
+
+       01 EDIT-SCREEN.
+           05 VALUE ALL " " PIC X(080) LINE 7 COL 08
+              BACKGROUND-COLOR 7.
+           05 VALUE ALL " " PIC X(080) LINE 22 COL 08
+              BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 08 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 09 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 10 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 11 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 12 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 13 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 14 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 15 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 16 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 17 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 18 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 19 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 20 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 21 COL 08 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 08 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 09 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 10 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 11 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 12 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 13 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 14 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 15 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 16 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 17 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 18 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 19 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 20 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 21 COL 86 BACKGROUND-COLOR 7.
+           05 VALUE SCREEN-INGREDS-ID LINE 12 COL 15.
+           05 VALUE MANUALLY-ADD-NAME LINE 14 COL 15.
+           05 VALUE MANUALLY-ADD-DESCRIPTION LINE 16 COL 15.
+           05 REGISTER-RECORD.
+               10 REG-INGRED-ID PIC 9(003) LINE 12 COL 29
+                   FROM WSINGREDS-ID.
+               10 REG-INGRED-NAME PIC X(030) LINE 14 COL 31
+                   FROM WSINGREDS-NAME REQUIRED.
+               10 REG-INGREDS-DESCRIPTION.
+                   15 REG-INGRED-DESCRIPTION PIC X(050) LINE 16 COL 27
+                       FROM WSINGREDS-DESCRIPTION REQUIRED AUTO.
+
+      ******************************************************************
+       01 LIST-FRAME.
+           05 VALUE ALL " " PIC X(082) LINE 7 COL 07
+              BACKGROUND-COLOR 7.
+           05 VALUE ALL " " PIC X(082) LINE 22 COL 07
+              BACKGROUND-COLOR 7.
+           05 VALUE LIST-FRAME1 LINE 08 COL 11 FOREGROUND-COLOR 5.
+           05 VALUE LIST-FRAME2 LINE 08 COL PLUS 4 FOREGROUND-COLOR 5.
+           05 VALUE LIST-FRAME1 LINE 08 COL 51 FOREGROUND-COLOR 5.
+           05 VALUE LIST-FRAME2 LINE 08 COL PLUS 4 FOREGROUND-COLOR 5.
+           05 VALUE "  " LINE 07 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 08 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 09 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 10 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 11 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 12 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 13 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 14 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 15 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 16 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 17 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 18 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 19 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 20 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 21 COL 07 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 07 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 08 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 09 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 10 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 11 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 12 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 13 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 14 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 15 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 16 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 17 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 18 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 19 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 20 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 21 COL 47 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 07 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 08 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 09 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 10 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 11 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 12 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 13 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 14 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 15 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 16 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 17 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 18 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 19 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 20 COL 87 BACKGROUND-COLOR 7.
+           05 VALUE "  " LINE 21 COL 87 BACKGROUND-COLOR 7.
+      ******************************************************************
+       01 UPDATE-RECORD.
+           05 EDIT-RECORD.
+               10 EDIT-INGREDS-NAME PIC X(030) LINE 09 COL 29
+                   TO WSINGREDS-NAME REQUIRED.
+               10 EDIT-INGREDS-DESCRIPTION.
+                   15 EDIT-INGRED-DESCRIPTION PIC X(050) LINE 10 COL 29
+                       TO WSINGREDS-DESCRIPTION REQUIRED AUTO.
+
+      ******************************************************************
+       01 EDIT-WHAT-SCREEN
+           BACKGROUND-COLOR 7 FOREGROUND-COLOR 0.
+           05 VALUE ALL " " PIC X(022) LINE 07 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 08 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 09 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 10 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 11 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 12 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 13 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 14 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 15 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 16 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 17 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 18 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 19 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 20 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 21 COL 98.
+           05 VALUE ALL " " PIC X(022) LINE 22 COL 98.
+           05 VALUE WHAT-TO-EDIT LINE 08 COL 103.
+           05 VALUE EDIT1 LINE 10 COL 100.
+           05 VALUE EDIT2 LINE 11 COL 100.
+           05 VALUE EDIT8 LINE 12 COL 100.
+           05 VALUE CHOOSE LINE 20 COL 100.
+           05 EDIT-CHOICE PIC 9(002) LINE 20 COL 117 BLANK WHEN ZERO
+               REQUIRED TO EDIT-WHAT.
+      ******************************************************************
+       01 ERROR-ZONE
+           BACKGROUND-COLOR 7 FOREGROUND-COLOR 0.
+           05 VALUE ALL " " PIC X(095) LINE 24 COL 01.
+           05 VALUE ALL " " PIC X(095) LINE 25 COL 01.
+           05 VALUE ALL " " PIC X(095) LINE 26 COL 01.
+           05 ERROR-TEXT LINE 25 COL 03 PIC X(085)
+               FOREGROUND-COLOR 4 BACKGROUND-COLOR 7.
+           05 SCREEN-DUMMY1 LINE 26 COL 95 PIC X TO DUMMY AUTO.
+      ******************************************************************
+       01 INSTRUCTIONS-ZONE
+           BACKGROUND-COLOR 7 FOREGROUND-COLOR 0.
+           05 VALUE ALL " " PIC X(095) LINE 24 COL 01.
+           05 VALUE ALL " " PIC X(095) LINE 25 COL 01.
+           05 VALUE ALL " " PIC X(095) LINE 26 COL 01.
+           05 INSTRUCTIONS-TEXT LINE 25 COL 03 PIC X(085)
+               FOREGROUND-COLOR 4 BACKGROUND-COLOR 7.
+      ******************************************************************
+       01 GET-INGREDID
+           BACKGROUND-COLOR 7 FOREGROUND-COLOR 0.
+           05 VALUE ALL " " PIC X(095) LINE 24 COL 01.
+           05 VALUE ALL " " PIC X(095) LINE 25 COL 01.
+           05 VALUE ALL " " PIC X(095) LINE 26 COL 01.
+           05 VALUE MESSAGE-GET-INGREDID LINE 25 COL 15
+               FOREGROUND-COLOR 4 BACKGROUND-COLOR 7.
+           05 VALUE " | " LINE 25 COL 46.
+           05 MESSAGE-LIST-PAGE LINE 25 COL 49 PIC X(030).
+           05 NEW-INGREDID LINE 25 COL 43 PIC 9(003)
+               FOREGROUND-COLOR 4 BACKGROUND-COLOR 7 TO GET-VALID-ID
+               BLANK WHEN ZERO.
+      ******************************************************************
+       01 INGREDIENT-LIST.
+           05 LIST-INGRED-ID PIC 9(003) LINE ILIN COL ICOL
+               FROM INGREDS-ID.
+           05 VALUE "|" LINE ILIN COL PLUS 1.
+           05 LIST-INGRED-NAME PIC X(030) LINE ILIN COL PLUS 1
+               FROM INGREDS-NAME.
+      ******************************************************************
+
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-            DISPLAY "Hello world"
-            STOP RUN.
-       END PROGRAM YOUR-PROGRAM-NAME.
+           PERFORM UNTIL INGREDEXIST-YES
+               PERFORM 100-INGREDIENT-LIST
+                   IF TRUE-YES = "Y" THEN
+                       EXIT PROGRAM
+                   END-IF
+               PERFORM 105-CHECK-IF-INGRED-ID-EXISTS
+           END-PERFORM
+           MOVE ZERO TO EDIT-WHAT
+           PERFORM WITH TEST AFTER UNTIL EDIT-WHAT = 8
+               PERFORM 110-EDIT-INGREDIENT
+               OPEN I-O FXINGRED
+                   PERFORM 115-EDIT-WHAT
+                   REWRITE INGREDS-DETAILS FROM WSINGREDS-DETAILS
+                   END-REWRITE
+               CLOSE FXINGRED
+           END-PERFORM
+           MOVE SPACES TO INGREDEXIST
+           EXIT PROGRAM.
 
+       100-INGREDIENT-LIST SECTION.
+           DISPLAY CLEAR-SCREEN
+           DISPLAY MAIN-SCREEN
+           DISPLAY LIST-FRAME
+           MOVE ZEROES TO NEW-INGREDID
+           MOVE SPACES TO TRUE-YES
+           MOVE 1 TO INGREDS-ID
+           OPEN INPUT FXINGRED
+           START FXINGRED KEY IS GREATER OR EQUAL INGREDS-ID
+               INVALID KEY
+                   MOVE EMPTY-LIST TO ERROR-TEXT
+                   ACCEPT ERROR-ZONE
+                   MOVE "Y" TO TRUE-YES
+                   EXIT SECTION
+           END-START
+           MOVE 09 TO ILIN
+           MOVE 11 TO ICOL
+           PERFORM UNTIL EOFINGREDS
+               READ FXINGRED NEXT RECORD
+                   AT END SET EOFINGREDS TO TRUE
+                   MOVE NO-MORE-INGREDS TO MESSAGE-LIST-PAGE
+                   ACCEPT GET-INGREDID
+                   EXIT SECTION
+                   IF KEYSTATUS = 1003 THEN
+                       EXIT SECTION
+                   END-IF
+                   NOT AT END
+                   DISPLAY INGREDIENT-LIST
+                   ADD 1 TO ILIN
+                   IF ILIN = 21 AND ICOL = 11 THEN
+                       MOVE 09 TO ILIN
+                       MOVE 51 TO ICOL
+                   ELSE
+                       IF ILIN = 21 AND ICOL = 51 THEN
+                           MOVE NEXT-PAGE TO MESSAGE-LIST-PAGE
+                           ACCEPT GET-INGREDID
+                           IF KEYSTATUS = 1002 THEN
+                               DISPLAY CLEAR-SCREEN
+                               DISPLAY MAIN-SCREEN
+                               DISPLAY LIST-FRAME
+                               MOVE 09 TO ILIN
+                               MOVE 11 TO ICOL
+                           ELSE
+                               EXIT SECTION
+                           END-IF
+                           IF KEYSTATUS = 1003
+                               EXIT SECTION
+                           END-IF
+                       END-IF
+                   END-IF
+               END-READ
+           END-PERFORM
+           EXIT SECTION.
+
+       105-CHECK-IF-INGRED-ID-EXISTS SECTION.
+           OPEN INPUT FXINGRED
+           MOVE GET-VALID-ID TO INGREDS-ID
+               READ FXINGRED INTO WSINGREDS-DETAILS
+                   NOT INVALID KEY
+                       MOVE "Y" TO INGREDEXIST
+                   INVALID KEY
+                       MOVE ERROR-INGREDID-NO TO ERROR-TEXT
+                       ACCEPT ERROR-ZONE
+               END-READ
+           CLOSE FXINGRED
+       EXIT SECTION.
+
+       110-EDIT-INGREDIENT SECTION.
+           DISPLAY CLEAR-SCREEN
+           DISPLAY MAIN-SCREEN
+           DISPLAY EDIT-SCREEN
+           ACCEPT EDIT-WHAT-SCREEN
+       EXIT SECTION.
+
+       115-EDIT-WHAT SECTION.
+           MOVE ZERO TO EDIT-CHOICE
+           EVALUATE EDIT-WHAT
+               WHEN 1
+                   PERFORM 150-GET-NAME
+               WHEN 2
+                   PERFORM 155-GET-DESCRIPTION
+           END-EVALUATE
+       EXIT SECTION.
+
+       150-GET-NAME SECTION.
+           MOVE MESSAGE-NAME TO INSTRUCTIONS-TEXT
+           DISPLAY INSTRUCTIONS-ZONE
+           MOVE WSINGREDS-NAME TO EDIT-INGREDS-NAME
+           ACCEPT EDIT-INGREDS-NAME
+           CALL "LOWERUPPER" USING BY REFERENCE WSINGREDS-NAME
+           MOVE TRIM(WSINGREDS-NAME) TO UNSTR
+           PERFORM 190-REMOVE-EXTRA-SPACES
+           MOVE UNSTR TO WSINGREDS-NAME
+           IF WSINGREDS-NAME EQUAL SPACES THEN
+               MOVE ERROR-NAME TO ERROR-TEXT ACCEPT ERROR-ZONE
+           END-IF
+       EXIT SECTION.
+
+       155-GET-DESCRIPTION SECTION.
+           MOVE MESSAGE-DESCRIPTION TO INSTRUCTIONS-TEXT
+           DISPLAY INSTRUCTIONS-TEXT
+           MOVE REG-INGRED-DESCRIPTION TO EDIT-INGREDS-DESCRIPTION
+           ACCEPT EDIT-INGRED-DESCRIPTION
+           CALL "LOWERUPPER" USING BY REFERENCE WSINGREDS-DESCRIPTION
+           MOVE TRIM(WSINGREDS-DESCRIPTION) TO UNSTR
+           PERFORM 190-REMOVE-EXTRA-SPACES
+           MOVE UNSTR TO WSINGREDS-DESCRIPTION
+       EXIT SECTION.
+
+       190-REMOVE-EXTRA-SPACES SECTION.
+           MOVE SPACE TO UNSTR1 UNSTR2 UNSTR3 UNSTR4 UNSTR5
+           UNSTR6 UNSTR7 UNSTR8 UNSTR9 UNSTR10
+           UNSTRING UNSTR DELIMITED BY ALL SPACES INTO UNSTR1
+               UNSTR2 UNSTR3 UNSTR4 UNSTR5 UNSTR6 UNSTR7 UNSTR8 UNSTR9
+               UNSTR10
+           STRING UNSTR1 DELIMITED BY SPACES SPACE DELIMITED BY SIZE
+                   UNSTR2 DELIMITED BY SPACES SPACE DELIMITED BY SIZE
+                   UNSTR3 DELIMITED BY SPACES SPACE DELIMITED BY SIZE
+                   UNSTR4 DELIMITED BY SPACES SPACE DELIMITED BY SIZE
+                   UNSTR5 DELIMITED BY SPACES SPACE DELIMITED BY SIZE
+                   UNSTR6 DELIMITED BY SPACES SPACE DELIMITED BY SIZE
+                   UNSTR7 DELIMITED BY SPACES SPACE DELIMITED BY SIZE
+                   UNSTR8 DELIMITED BY SPACES SPACE DELIMITED BY SIZE
+                   UNSTR9 DELIMITED BY SPACES SPACE DELIMITED BY SIZE
+                   UNSTR10 DELIMITED BY SPACES SPACE DELIMITED BY SIZE
+           INTO UNSTR
+       EXIT SECTION.
