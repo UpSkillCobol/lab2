90232de1eb681f2ec6b125d13c487bee62da5148
cscosta95 <75365445+cscosta95@users.noreply.github.com>
2021-03-04 10:14:01 +0000

    Advanced changes on Main
    Close to finished

--- Part3/src/SR/SR-MAIN.cbl
+++ Part3/src/SR/SR-MAIN.cbl
@@ -8,6 +8,8 @@
        PROGRAM-ID. SR-MAIN.
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+           CRT STATUS IS KEY-STATUS.
        REPOSITORY.
            FUNCTION ALL INTRINSIC.
        DATA DIVISION.
@@ -19,7 +21,7 @@
        01  CLEAR-SCREEN BACKGROUND-COLOR 0.
            03 VALUE " " BLANK SCREEN LINE 01 COL 01.
       ******************************************************************
-       01  MAIN-SCREEN.
+       01  MAIN-SCREEN
            BACKGROUND-COLOR 7, FOREGROUND-COLOR 0.
            05 VALUE ALL " " PIC X(120) LINE 02 COL 01.
            05 VALUE ALL " " PIC X(120) LINE 03 COL 01.
@@ -42,6 +44,8 @@
            03 VALUE ALL " " PIC X(50) LINE 17 COL 35.
            03 VALUE MAIN-MENU-OPTION1 LINE 11 COL 50.
            03 VALUE MAIN-MENU-OPTION2 LINE 12 COL 50.
+           03 VALUE MAIN-MENU-OPTION3 LINE 13 COL 50.
+           03 VALUE MAIN-MENU-OPTION4 LINE 14 COL 50.
            03 VALUE MAIN-MENU-CHOICE LINE 20 COL 45
            REVERSE-VIDEO.
            03 MP-OPTION PIC 9(02) LINE 20 COL 73 TO WS-OPTION
@@ -61,7 +65,39 @@
            05 VALUE LEAVE-THANKS LINE 11 COL 45.
       ******************************************************************
        PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
-            DISPLAY "Hello world"
-            STOP RUN.
+       MAIN SECTION.
+      *    MAIN SECTION WHERE THE USER DECIDES WHAT TO DO, THE DECISION
+      *    TRIGGERS A CALL OF A MODULE RESPONSIBLE FOR THE OPTION THAT
+      *    THE USER DID CHOOSE
+           PERFORM WITH TEST AFTER UNTIL WS-OPTION = 4
+           MOVE ZEROS TO KEY-STATUS
+           MOVE ZERO TO MP-OPTION
+           DISPLAY CLEAR-SCREEN
+           DISPLAY MAIN-SCREEN
+           DISPLAY MAIN-MENU
+           ACCEPT MP-OPTION
+           EVALUATE WS-OPTION
+               WHEN 1
+      *    OPTION 1 CALLS ADD MODULE THAT REGISTERS NEW RECORDS ON THE FILE
+                       CALL "SR-ADD"
+                       IF KEY-STATUS = F3 THEN
+                           EXIT PROGRAM
+                       END-IF
+               WHEN 2
+      *    OPTION 2 CALLS VIEW MODULE THAT ALLOWS THE USER TO VIEW THE RECORDS
+      *    ALREADY REGISTERED ON THE FILE
+                       CALL "SR-VIEW"
+                       IF KEY-STATUS = F3 THEN
+                           EXIT PROGRAM
+                       END-IF
+               WHEN 3
+      *    OPTION 3 CALLS REP MODULE THAT CREATES A REPORT OF ALL THE
+      *    RECORDS ON THE FILE
+                       CALL "SR-REP"
+                       IF KEY-STATUS = F3 THEN
+                           EXIT PROGRAM
+                       END-IF
+           END-EVALUATE
+           END-PERFORM
+           EXIT PROGRAM.
        END PROGRAM SR-MAIN.
