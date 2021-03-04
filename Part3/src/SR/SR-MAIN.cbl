fc13318283a5d5d61be9e8c8d9db2ce12b40a757
cscosta95 <75365445+cscosta95@users.noreply.github.com>
2021-03-04 09:39:35 +0000

    Create SR-MAIN.cbl

---
+++ Part3/src/SR/SR-MAIN.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: BreadWich
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SR-MAIN.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM SR-MAIN.
